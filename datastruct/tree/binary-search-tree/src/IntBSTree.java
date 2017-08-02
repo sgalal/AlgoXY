312b8e48386bf76d9ae402e75bec782ff11fd53b
Liu Xinyu <liuxinyu95@gmail.com>
2017-08-02 22:11:47 +0800

    Added min/max/pred/succ in Java example

--- datastruct/tree/binary-search-tree/src/IntBSTree.java
+++ datastruct/tree/binary-search-tree/src/IntBSTree.java
@@ -48,6 +48,42 @@ public class IntBSTree {
         return tr;
     }
 
+    public static Node min(Node t) {
+        while (t != null && t.left != null)
+            t = t.left;
+        return t;
+    }
+
+    public static Node max(Node t) {
+        while (t != null && t.right != null)
+            t = t.right;
+        return t;
+    }
+
+    // assume the input x isn't null when find succ/pred
+
+    public static Node succ(Node x) {
+        if (x.right != null)
+            return min(x.right);
+        Node p = x.parent;
+        while (p != null && p.left != x) {
+            x = p;
+            p = p.parent;
+        }
+        return p;
+    }
+
+    public static Node pred(Node x) {
+        if (x.left != null)
+            return max(x.left);
+        Node p = x.parent;
+        while (p != null && p.right != x) {
+            x = p;
+            p = p.parent;
+        }
+        return p;
+    }
+
     // verification
 
     static Node fromList(Collection<Integer> xs) {
@@ -99,12 +135,42 @@ public class IntBSTree {
         }
     }
 
+    static void testMinMax(List<Integer> xs) {
+        final Node t = fromList(xs);
+        if (t == null) return;
+        if (!Collections.min(xs).equals(min(t).key))
+            throw new RuntimeException(String.format("min(xs)=%d, min(tree)=%d",
+                                                 Collections.min(xs), min(t).key));
+        if (!Collections.max(xs).equals(max(t).key))
+            throw new RuntimeException(String.format("max(xs)=%d, min(tree)=%d",
+                                                 Collections.max(xs), max(t).key));
+    }
+
+    static void testSuccPred(List<Integer> xs) {
+        final Node t = fromList(xs);
+        List<Integer> ys = xs.stream().sorted().collect(Collectors.toList());
+        for (int i = 0; i < xs.size(); ++i) {
+            Node x = search(t, ys.get(i));
+            Node y = succ(x), z = pred(x);
+            if (i == 0 && z != null)
+                throw new RuntimeException("succ(first) != null");
+            if (i == xs.size() - 1 && y != null)
+                throw new RuntimeException("pred(last) != null");
+            if (i > 0 && z.key != ys.get(i - 1))
+                throw new RuntimeException(String.format("pred(%d) = %d != %d", x.key, z.key, ys.get(i - 1)));
+            if (i != xs.size() - 1 && y.key != ys.get(i + 1))
+                throw new RuntimeException(String.format("succ(%d) = %d != %d", x.key, y.key, ys.get(i + 1)));
+        }
+    }
+
     public static void main(String[] args) {
         final Random gen = new Random();
         for (int i = 0; i < 100; ++i) {
             final List<Integer> xs = genList(gen);
             testBuild(xs);
             testSearch(xs, gen.nextInt(N));
+            testMinMax(xs);
+            testSuccPred(xs);
         }
         System.out.println("passed 100 tests.");
         traverse(fromList(genList(gen)), System.out::println);
