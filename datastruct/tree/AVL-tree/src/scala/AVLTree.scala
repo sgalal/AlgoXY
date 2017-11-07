0fbd18e48abbc240587cecf7ad5a4fbe207e4001
Liu Xinyu <liuxinyu95@gmail.com>
2017-11-07 10:14:52 +0800

    Replaced AVL ins with Scala example programs

--- datastruct/tree/AVL-tree/src/scala/AVLTree.scala
+++ datastruct/tree/AVL-tree/src/scala/AVLTree.scala
@@ -9,15 +9,15 @@ object AVLTree {
 
   def insert[A <% Ordered[A]] (tr: Tree[A], x: A): Tree[A] = ins(tr, x)._1
 
-  // returns (t, d), where t: tree, d: increment of of height
+  // returns (t, d), where t: tree, d: delta of height
   def ins[A <% Ordered[A]] (tr: Tree[A], x: A): (Tree[A], Int) = tr match {
     case Empty => (Br(Empty, x, Empty, 0), 1)
     case Br(l, k, r, d) => if (x == k) (Br(l, k, r, d), 0)
-      else if (x < k) node(ins(l, x), k, (r, 0), d)
-      else node((l, 0), k, ins(r, x), d)
+      else if (x < k) tree(ins(l, x), k, (r, 0), d)
+      else tree((l, 0), k, ins(r, x), d)
   }
 
-  def node[A] (left: (Tree[A], Int), key: A, right: (Tree[A], Int), d: Int): (Tree[A], Int) = {
+  def tree[A] (left: (Tree[A], Int), key: A, right: (Tree[A], Int), d: Int): (Tree[A], Int) = {
     val d1 = d + right._2 - left._2
     val delta = deltaH(d, d1, left._2, right._2)
     balance(Br(left._1, key, right._1, d1), delta)
